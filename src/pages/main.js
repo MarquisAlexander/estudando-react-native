e5c3c82e62d7158b07d7509a3cec700a8b1ef3f1
MarquisAlexander <51330232+MarquisAlexander@users.noreply.github.com>
2019-09-16 17:31:58 -0300

    parametrizacao para buscar dados no backend

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text } from "react-native";
+import api from "../services/api";
 
-export default class Main extends Component {
+    export default class Main extends Component {
     static navigationOptions ={
-        title: "projeto",
-    }
+        title: "                                          projeto",
+    };
+
+    state = {
+        docs: [],
+    };
+
+    componentDidMount() {
+        this.loadProducts();
+    };
+
+    loadProducts = async() => {
+        const response = await api.get("/products");
+        
+        const { docs } = response.data;
+
+        this.setState({ docs });
+    };
+
     render(){
         return(
             <View>
-                <Text>Página-Main</Text>
+                <Text>Página-Main:</Text>
+                {this.state.docs.map( product => (
+                    <Text key={product._id}>{ product.title }</Text>
+                ))}
             </View>
         );
     }
