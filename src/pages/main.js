714a4ef84d8a6b5af4450be5fece1d712e941a14
MarquisAlexander <51330232+MarquisAlexander@users.noreply.github.com>
2019-09-17 17:00:14 -0300

    adicionando funcionalidades para montrar em tela os produtos enviados do back end e estilização da página

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,36 +1,122 @@
 import React, { Component } from 'react';
-import { View, Text } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from "react-native";
 import api from "../services/api";
 
     export default class Main extends Component {
     static navigationOptions ={
-        title: "                                          projeto",
+        title: "                             Sobre React-native",
     };
 
+// armazenando status dos arquivos enviados do back end
     state = {
+        productInfo: {},
         docs: [],
+        page: 1,
     };
 
+// Carregando produtos do back end
     componentDidMount() {
         this.loadProducts();
     };
 
-    loadProducts = async() => {
-        const response = await api.get("/products");
+    loadProducts = async(page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
         
-        const { docs } = response.data;
+        const { docs, ...productInfo } = response.data;
 
-        this.setState({ docs });
+        this.setState({ 
+            docs: [...this.state.docs, ...docs], 
+            productInfo,
+            page
+         });
     };
 
-    render(){
-        return(
-            <View>
-                <Text>Página-Main:</Text>
-                {this.state.docs.map( product => (
-                    <Text key={product._id}>{ product.title }</Text>
-                ))}
+    renderItem = ({ item }) => (
+        <View style={styles.productContainer}>
+            <Text style={styles.productTitle}>{item.title}</Text>
+            <Text style={styles.productDescription}>{item.description}</Text>
+
+            <TouchableOpacity style={styles.productButton} onPress={() => {}}>
+                <Text style={styles.productButtonText}>Acessar</Text>
+            </TouchableOpacity>
+        </View>
+    );
+
+//func para parar o carregamento de novas páginas quando elas chegarem ao fim
+    loadMore = () => {
+        const {page, productInfo} = this.state;
+
+        if (page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    };
+
+//renderizando em tela os produtos do back end
+    render() {
+        return (
+            <View style={styles.container}>
+                <FlatList 
+                contentContainerStyle={styles.list}
+                    data={this.state.docs}
+                    keyExtractor={item => item._id}
+                    renderItem={this.renderItem}
+//funcionalidade para carregar mais produtos em tela, a medida que o usuário for descendo o scroll
+                    onEndReached={this.loadMore}
+                    onEndReachedThreshold={0.1}
+                />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fafafa"
+    },
+
+    list: {
+        padding: 20
+    },
+
+    productContainer: {
+        backgroundColor: "#FFF",
+        borderWidth: 1,
+        borderColor: "#DDD",
+        borderRadius: 5,
+        padding: 20,
+        marginBottom: 20
+    },
+
+    productTitle: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "#333"
+    },
+
+    productDescription: {
+        fontSize: 16,
+        color: "#999",
+        marginTop: 5,
+        lineHeight: 24
+    },
+
+    productButton: {
+        height: 42,
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor:"#7129c2",
+        backgroundColor: "#7129c2",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 10
+    },
+
+    productButtonText: {
+        fontSize: 16,
+        color: "#FFF",
+        fontWeight: "bold"
+    },
+}); 
