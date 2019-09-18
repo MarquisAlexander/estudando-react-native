96fc08422ff4bcdf4999b2a94615ae0640ba84c0
Werdeles Marcio de C. Soares <36682515+werdelesmarcio@users.noreply.github.com>
2019-09-18 20:49:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ import App from './src';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+//Marquis faz o arquivo README explicando o que é esse repo
